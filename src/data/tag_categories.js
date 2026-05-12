306828531f323b3dd394709629e22671b33798d5
jiachenhu02 <164726559+jiachenhu02@users.noreply.github.com>
2026-05-12 22:55:22 +0800

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,37 @@
 const userDefinedTagCategories = {
-	"type": {
-		"description": "type of the paper"
-	}
+  "type": {
+    "description": "type of the paper"
+  },
+  "engagement_recognition": {
+    "description": "classroom engagement recognition",
+    "color": "#1a6496"
+  },
+  "face_anonymisation": {
+    "description": "face anonymisation methods",
+    "color": "#c0392b"
+  },
+  "privacy_perception": {
+    "description": "privacy perception and chilling effect",
+    "color": "#27ae60"
+  },
+  "sem_tam": {
+    "description": "SEM and technology acceptance frameworks",
+    "color": "#8e44ad"
+  },
+  "chilling_effect": {
+    "description": "chilling effect and surveillance",
+    "color": "#e67e22"
+  },
+  "deep_learning": {
+    "description": "deep learning methods",
+    "color": "#2980b9"
+  },
+  "classroom": {
+    "description": "classroom settings",
+    "color": "#16a085"
+  },
+  "dataset": {
+    "description": "dataset contribution",
+    "color": "#95a5a6"
+  }
 }
